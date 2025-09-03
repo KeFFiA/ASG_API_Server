ceced0ab8220dcb92a169b0a67a7637912b91da8
KeFFiA <ogienko.12003@gmail.com>
2025-09-03 16:32:04 +0300

    Update v0.1.0

--- src/Database/Models.py
+++ src/Database/Models.py
@@ -9,5 +9,6 @@ class PDF_Queue(Base):
     type: Mapped[str] = mapped_column(String, nullable=False)
     queue_position: Mapped[int] = mapped_column(Integer, nullable=False)
     status: Mapped[str] = mapped_column(String, nullable=False, default="Queued")
+    status_description: Mapped[str] = mapped_column(String, nullable=True)
     user_email: Mapped[str] = mapped_column(String, nullable=False)
 
