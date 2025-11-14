e67ed5571aef1c5a3af87ce755f7e41014c96dfe
Alexander <80920304+KeFFiA@users.noreply.github.com>
2025-11-14 13:04:12 +0300

    Update | Add get_db endpoint

--- src/Routers/__init__.py
+++ src/Routers/__init__.py
@@ -3,3 +3,4 @@ from .Root import router as root
 from .StatusCheck import router as status
 from .Webhook import router as webhook
 from .MSGraph import router as msgraph
+from .Database import router as database
