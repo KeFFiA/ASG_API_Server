91a133dfeec64c9e9916a68e5cc4e5c451b06c67
KeFFiA <ogienko.12003@gmail.com>
2026-01-07 19:59:42 +0300

    Update v0.3.5 | Add Scheduler for Airlabs Flights API

--- src/Config/config.py
+++ src/Config/config.py
@@ -84,7 +84,7 @@ SELF_PORT: int = require_env("SELF_PORT", 8000)
 
 API_TITLE: str = require_env("API_TITLE", "AIXII API Server")
 API_DESCRIPTION: str = require_env("API_DESCRIPTION", "")
-API_VERSION: str = require_env("API_VERSION", "0.3.2")
+API_VERSION: str = require_env("API_VERSION", "0.3.5")
 API_SWAGGER_URL: str = require_env("API_SWAGGER_URL", "/api/docs")
 API_REDOC_URL: str = require_env("API_REDOC_URL", "/api/redoc")
 API_ROOT_URL: str = require_env("API_ROOT_URL", "/api/v1")
