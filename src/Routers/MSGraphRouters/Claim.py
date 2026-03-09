e0979528852c95fdf1a8656332e7b0288409921f
KeFFiA <ogienko.12003@gmail.com>
2026-03-09 10:24:19 +0300

    Update | v0.5.4 | Added delete claim endpoint

--- src/Routers/MSGraphRouters/Claim.py
+++ src/Routers/MSGraphRouters/Claim.py
@@ -61,8 +61,8 @@ async def get_claims(request: Request, _payload: Annotated[ClaimsQuery, Query()]
             return success_response(request=request, data=airline_data, msg="Claim(-s) retrieved successfully")
         return warning_response(request=request, msg="Claim(-s) not found", status_code=status.HTTP_404_NOT_FOUND)
 
-    # except ValueError:
-    #     return warning_response(request=request, msg="Claim(-s) not found", status_code=status.HTTP_404_NOT_FOUND)
+    except ValueError:
+        return warning_response(request=request, msg="Claim(-s) not found", status_code=status.HTTP_404_NOT_FOUND)
     except Exception as _ex:
         logger.error(f"Failed to get Claim(-s): {_ex}")
         return error_response(request=request, exc=_ex)
@@ -82,9 +82,9 @@ async def create_claim(request: Request, _payload: Annotated[CreateClaimBodySche
             return await query_create_claim(session, _payload)
 
         if _payload.user_id:
-            cache_key = f"claims:{_payload.user_id}"
+            cache_key = f"_claims:{_payload.user_id}"
         else:
-            cache_key = f"claims:{_payload.claim_id}"
+            cache_key = f"_claims:{_payload.claim_id}"
 
         result = await db_proxy.update_and_cache(
             key=cache_key,
